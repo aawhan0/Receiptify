9bbafd8fd0b8d6e165296aa194c053015b27726c
Aawhan <114390114+aawhan0@users.noreply.github.com>
2025-09-20 08:46:49 +0530

    preprocessing func added

--- utils/ocr.py
+++ utils/ocr.py
@@ -1,6 +1,19 @@
-# utils/ocr.py
-import pytesseract
+import cv2
+import numpy as np
 from PIL import Image
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'  # Only needed on Windows
+
+def preprocess_image(image_path):
+    image = cv2.imread(image_path)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (5,5), 0)
+    _, thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    kernel = np.ones((2,2), np.uint8)
+    processed = cv2.dilate(thresh, kernel, iterations=1)
+    return processed
 
 def extract_text(image_path):
     image = Image.open(image_path)
